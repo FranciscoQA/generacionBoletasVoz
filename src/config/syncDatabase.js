6d765376a7849e23bc2f00dede6b5b84c407674d
Francisco Ronaldo Jimenez Ramirez <ronaldo.jimenez@encora.com>
2025-04-10 20:07:38 -0500

    databse

--- src/config/syncDatabase.js
+++ src/config/syncDatabase.js
@@ -6,20 +6,11 @@ const syncDatabase = async () => {
     try {
         await sequelize.sync({ force: true }); // Esto borra y recrea las tablas cada vez (solo para desarrollo)
         console.log('Tablas sincronizadas con la base de datos.');
-    } catch (err) {
-        console.error('Error al sincronizar las tablas:', err);
-    }
-};
- 
-syncDatabase();
-
-const syncDatabase = async () => {
-    try {
-        await sequelize.sync({ force: true });
-        console.log('Tablas sincronizadas con la base de datos.');
         const [results] = await sequelize.query(`SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';`);
         console.log('Tablas existentes:', results);
     } catch (err) {
         console.error('Error al sincronizar las tablas:', err);
     }
 };
+ 
+syncDatabase();
